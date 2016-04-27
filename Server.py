b7d8ff92622e7de623c4c9a2ab8436a756f25452
J <jimfinoc@gmail.com>
2016-04-26 20:30:37 -0400

    adds in threaded code

--- Server.py
+++ Server.py
@@ -3,6 +3,9 @@ import os
 import platform
 import glob
 import sys
+import thread
+import time
+
 
 ## This lets us know if we are running the program on a Mac or Linux Computer.
 #print platform.system()
@@ -28,7 +31,27 @@ else:
     print "Your Arduino is at",
     print ardunioLocation[0]
 
+def receiveFromArduino (threadName, delay):
+    time.sleep(delay)
+    print "%s: %s" % ( threadName, time.ctime(time.time()) )
+    while True:
+        print ser.readline(),
+
+def sendToArduino (threadName, delay):
+    time.sleep(delay)
+    print "%s: %s" % ( threadName, time.ctime(time.time()) )
+    while True:
+        inputData = raw_input('Enter command: ')
+        ser.write(inputData.encode('utf-8'))
+
+
 
 ser = serial.Serial(ardunioLocation[0], 115200)
-while True:
-	print ser.readline(),
+try:
+    thread.start_new_thread( receiveFromArduino, ("Thread-1", 2, ) )
+    thread.start_new_thread( sendToArduino, ("Thread-2", 4, ) )
+except:
+    print "Error: unable to start thread"
+
+while 1:
+    pass
