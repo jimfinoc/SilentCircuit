257a5fb883d961fe2655129f04a1d67e79fb458d
J <jimfinoc@gmail.com>
2016-04-30 12:37:06 -0400

    commenting out db write

--- Server.py
+++ Server.py
@@ -88,10 +88,11 @@ try:
 except:
     print "Error: unable to start thread"
 
-print "Before sleep and write to DB"
-time.sleep(10)
-wt2Sqlite3(123456789012345,100,100)
-print "After sleep and write to DB"
+# testing the database
+# print "Before sleep and write to DB"
+# time.sleep(6)
+# wt2Sqlite3(123456789012345,100,100)
+# print "After sleep and write to DB"
 
 while 1:
     pass
