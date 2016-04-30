97d35b75c0d91158407f69b963f3ca4904d715f0
J <jimfinoc@gmail.com>
2016-04-29 21:17:26 -0400

    Merged Code

--- Server.py
+++ Server.py
@@ -3,6 +3,9 @@ import os
 import platform
 import glob
 import sys
+import thread
+import time
+
 
 ## This lets us know if we are running the program on a Mac or Linux Computer.
 #print platform.system()
@@ -18,7 +21,7 @@ print "I hope you are using a", computerSystem
 if computerSystem == 'Mac':
     typeFile = "/dev/cu.usbmodem*"
 elif computerSystem == 'Raspi':
-    typeFile = "/dev/ttyAMC*"
+    typeFile = "/dev/ttyACM*"
 
 ardunioLocation = glob.glob(typeFile)
 if not ardunioLocation:
@@ -28,8 +31,23 @@ else:
     print "Your Arduino is at",
     print ardunioLocation[0]
 
+def receiveFromArduino (threadName, delay):
+    time.sleep(delay)
+    print "%s: %s" % ( threadName, time.ctime(time.time()) )
+    while True:
+        print ser.readline(),
+
+def sendToArduino (threadName, delay):
+    time.sleep(delay)
+    print "%s: %s" % ( threadName, time.ctime(time.time()) )
+    while True:
+        inputData = raw_input('Enter command: ')
+        ser.write(inputData.encode('utf-8'))
+
+
 
 ser = serial.Serial(ardunioLocation[0], 115200)
+<<<<<<< HEAD
 while True:
 	print ser.readline(),
 
@@ -56,3 +74,13 @@ coord4 = [70.0000, -80.0000]
 wt2Sqlite3(coord4)
 coord5 = [90.0000, -10.0000]
 wt2Sqlite3(coord5)
+=======
+try:
+    thread.start_new_thread( receiveFromArduino, ("Thread-1", 2, ) )
+    thread.start_new_thread( sendToArduino, ("Thread-2", 4, ) )
+except:
+    print "Error: unable to start thread"
+
+while 1:
+    pass
+>>>>>>> master
